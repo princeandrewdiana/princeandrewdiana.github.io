2a0d50d7db379afa29281ef638168b16cd7e3861
princeandrewdiana <princediana11@gmail.com>
2022-09-09 23:13:51 +0800

    Make page responsive for mobile

--- src/components/WorkScreen.jsx
+++ src/components/WorkScreen.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import { Parallax } from 'react-scroll-parallax';
 import { Fade } from "react-awesome-reveal";
 
@@ -40,24 +40,17 @@ const ProjectImages = ({images, setProgress, setCurrentProject}) => {
                     <img style={{height: '90vh'}} src={images[0]} />
                 </div>
 
-                <div class="flex justify-end" 
-                style={{marginTop: '15vh'}}
-                >
+                <div class="flex justify-end" style={{marginTop: '15vh'}}>
                     <img style={{height: '85vh'}} src={images[1]} />
                 </div>
 
-                    <div class="flex justify-center" 
-                    style={{marginTop: '-20vh'}}
-                    >
-                        <img style={{height: '65vh'}} src={images[2]} />
-                    </div>
-
+                <div class="flex justify-center" style={{marginTop: '-20vh'}}>
+                    <img style={{height: '65vh'}} src={images[2]} />
+                </div>
 
                 {
                     images[3] &&
-                            <div class="flex justify-center" 
-                            style={{marginTop: '-10vh'}}
-                            >
+                            <div class="flex justify-center" style={{marginTop: '-10vh'}}>
                                 <img style={{height: '65vh'}} src={images[3]} />
                             </div>
                 }
@@ -71,23 +64,29 @@ const ProjectImages = ({images, setProgress, setCurrentProject}) => {
 
 const ProjectDetails = ({project, number}) => {
     return(
-        <div>
-            <p class="absolute text-[3.5vh] font-semibold" style={{padding: 60}}>0{number}</p>
-            <div class="grid h-screen place-items-center">
-                <div class="flex flex-col gap-2" style={{padding: 60, width: '100%'}}>
-                    <p class="text-[5.9vw]">{project?.title}</p>
-                    <p class="text-[4.0vh]">{project?.roles}</p>
-                    <p class="text-[3.0vh]">{project?.description}</p>
+        <div class="pb-2 sm:pb-0
+            bg-white bg-opacity-50 backdrop-blur-xl rounded drop-shadow-lg 
+            sm:bg-none sm:bg-opacity-0 sm:backdrop-blur-none sm:rounded-none sm:drop-shadow-none">
+       
+            <p class="absolute font-semibold text-[5vw] sm:text-[2.5vw] p-6 sm:p-[60px]">0{number}</p>
+        
+            <div class="grid sm:h-screen sm:place-items-center ">
+                <div class="flex flex-col gap-2 p-6 sm:p-[60px] mt-10 sm:mt-0 mb-[-15px] w-full">
+                    <p class="text-[7.5vw] sm:text-[5.9vw]">{project?.title}</p>
+                    <p class="text-[3.5vw] sm:text-[2.0vw]">{project?.roles}</p>
+                    <p class="text-[3.0vw] sm:text-[1.5vw]">{project?.description}</p>
                 </div>
             </div>
-            <div class="absolute bottom-0 left-0" style={{padding: 60}}>
+
+            <div class="sm:absolute sm:bottom-0 sm:left-0 pl-6 sm:p-[60px] w-full ">
                 <div class="flex flex-row gap-2">
-                    <p class="text-[3.0vh] font-semibold">{project?.platforms} | </p>
+                    <p class="text-[3vw] sm:text-[1.5vw] font-semibold">{project?.platforms} | </p>
                     <a href={project?.platforms_link?.android} target="_blank" rel="noreferrer">
-                        <img style={{cursor: 'pointer', height: '3.2vh', marginTop: '1vh'}} src={PlaystoreIcon} />
+                        <img class="h-[3vw] sm:h-[1.5vw] mt-[0.5vw]" style={{cursor: 'pointer'}} src={PlaystoreIcon} />
                     </a>
                 </div>
             </div>
+
         </div>
     )
 }
@@ -143,7 +142,7 @@ const WorkScreen = () => {
                                 <p class="text-[8.9vw]">Prince Diana</p>
                             </div>
                             <div>
-                                <p class="text-[6.0vh]">Software Developer & Full Stack Developer</p>
+                                <p class="text-[3.0vw]">Software Developer & Full Stack Developer</p>
                             </div>
                         </Fade>
                     </div>
@@ -153,27 +152,25 @@ const WorkScreen = () => {
             <div style={{height: '100vh', width: '100%'}}>
                 <div class="grid h-screen place-items-center">
                     <div class="grid place-items-center">
-                        <p class="text-[6.0vh]">I create mobile, web, and back-end applications</p>
-                        <p class="text-[6.0vh]">with serious passion for UI/UX design & new technologies</p>
+                        <p class="text-[3.25vw]">I create mobile, web, and back-end applications</p>
+                        <p class="text-[3.25vw]">with serious passion for UI/UX design & new technologies</p>
                     </div>
                 </div>
             </div>
         
             <div class="flex flex-row">
-                
-                <div class="relative">
 
-                    <div class="basis-1/2" style={{position: 'fixed', width: '50%', height: '100vh', top: '0'}}>
+                <div class="relative z-10 sm:z-0">
+
+                    <div class="sm:basis-1/2 w-full sm:w-6/12" style={{position: 'fixed', height: '100vh', top: '0'}}>
                         { progress >= 0.2 && <Fade direction='left'><ProjectDetails number={current_project_number} project={current_project}/></Fade> }
                     </div>
 
                 </div>
 
-                {/* <div class="basis-1/2"/> */}
-                <div class="basis-1/2">
-                </div>
+                <div class="sm:basis-1/2"/>
 
-                <div class="basis-1/2">
+                <div class="sm:basis-1/2">
                     { projects.map((project, index)=>{
                         return(
                             <div key={project.title}>
